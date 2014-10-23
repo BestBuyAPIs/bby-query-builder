0728e72ad84f5976f6ef51ff044883b7dde4d471
kurt.christensen <kurt.christensen@bestbuy.com>
2014-10-23 15:41:45 -0500

    Adding some additional helpful criteria to the product search query builder

--- app/productSearch/productSearch.js
+++ app/productSearch/productSearch.js
@@ -7,12 +7,17 @@ angular.module('bby-query-mixer.productSearch').controller('ProductSearchCtrl', 
         $scope.apiKey = '';
         $scope.categories = angular.copy(categoryConfig);
         $scope.category = $scope.categories[0];
+        $scope.sortBy = 'none';
+        $scope.sortOrder = 'asc';
+
         $scope.buildRemixQuery = function () {
             var categorySearchCriterion = $scope.category.value ? '(categoryPath.id=' + $scope.category.value + ')' : '';
             var apiKeyQueryString = $scope.apiKey ? '?apiKey=' + $scope.apiKey : '';
+            var sortByQueryString = ($scope.apiKey && $scope.sortBy && $scope.sortBy != 'none') ? '&sort=' + $scope.sortBy : '';
+            var sortOrderQueryString = ($scope.apiKey && $scope.sortBy && $scope.sortBy != 'none' && $scope.sortOrder) ? '.' + $scope.sortOrder : '';
 
             if (categorySearchCriterion || apiKeyQueryString) {
-                return 'https://api.remix.bestbuy.com/v1/products' + categorySearchCriterion + apiKeyQueryString;
+                return 'https://api.remix.bestbuy.com/v1/products' + categorySearchCriterion + apiKeyQueryString + sortByQueryString + sortOrderQueryString;
             }
 
             return '';

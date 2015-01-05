4ca86a30a14f9132a6fec39f3dae5ddf5daceacf
Christopher Lizama <Christopher.Lizama@gmail.com>
2015-01-05 15:33:31 -0600

    show param checkboxes now also update the query string

--- app/productSearch/productSearch.js
+++ app/productSearch/productSearch.js
@@ -10,6 +10,28 @@ angular.module('bby-query-mixer.productSearch').controller('ProductSearchCtrl', 
         $scope.sortBy = 'none';
         $scope.sortOrder = 'asc';
 
+        $scope.showOptions = [
+            { text: 'sku', value: 'sku' },
+            { text: 'name', value: 'name' },
+            { text: 'price', value: 'price' },
+            { text: 'description', value: 'description' },
+            { text: 'weight', value: 'weight' },
+            { text: 'sale price', value: 'sale price' },
+            { text: 'batteries included', value: 'batteries included' },
+            { text: 'customer review average', value:'customer review average' }
+        ];
+
+        $scope.option = {
+            showOptions: ['sku', 'name']
+        };
+
+        $scope.showMyOptions = $scope.option.showOptions.join(',');
+
+
+        $scope.showOpts = function () {
+            console.log($scope.option.showOptions);
+        };
+
         $scope.buildRemixQuery = function () {
             var baseUrl = 'https://api.remix.bestbuy.com/v1/products' + ($scope.category.value ? '(categoryPath.id=' + $scope.category.value + ')' : '');
             return baseUrl + $scope.buildParams();
@@ -26,6 +48,8 @@ angular.module('bby-query-mixer.productSearch').controller('ProductSearchCtrl', 
                 paramArgs.push('sort=' + $scope.sortBy + '.' + $scope.sortOrder);
             }
 
+            paramArgs.push('&show=' + $scope.option.showOptions.join(','));
+
             paramArgs.push('format=json');
 
             if (paramArgs.length > 0) {
@@ -35,24 +59,6 @@ angular.module('bby-query-mixer.productSearch').controller('ProductSearchCtrl', 
             }
         };
 
-        $scope.showOptions = [
-            { text: 'SKU', value: 'SKU' },
-            { text: 'name', value: 'name' },
-            { text: 'price', value: 'price' },
-            { text: 'description', value: 'description' },
-            { text: 'weight', value: 'weight' },
-            { text: 'sale price', value: 'sale price' },
-            { text: 'batteries included', value: 'batteries included' },
-            { text: 'customer review average', value:'customer review average' }
-        ];
-
-        $scope.option = {
-            showOptions: ['SKU', 'name']
-        };
-
-        $scope.showOpts = function () {
-            console.log($scope.option.showOptions);
-        };
 
     }
 ]);

c6cd1f88952303836426f72cf15e7cbc0bc0722f
Chris Lizama <christopher.lizama@gmail.com>
2015-01-26 15:30:48 -0600

    change icons + attr operator now preselects

--- app/openBox/openBox.js
+++ app/openBox/openBox.js
@@ -3,7 +3,8 @@
 angular.module('bby-query-mixer.openBox').controller('openBoxCtrl', [
     '$scope',
     '$resource',
-    function ($scope, $resource) {
+    'categoryConfig',
+    function ($scope, $resource, categoryConfig) {
 
         var httpClient = function (query) {
             return $resource(query, {}, {
@@ -13,6 +14,21 @@ angular.module('bby-query-mixer.openBox').controller('openBoxCtrl', [
             });
         };
 
+        $scope.options = [
+            {text: "Select an Open Box Search Option"},
+        	{text: "Open Box Offers All SKUs"},
+        	{text: "Open Box Offers by Category"},
+        	{text: "Open Box Offers by List of SKUs"},
+        	{text: "Open Box Offers by SKU"}
+        ];
+        
+        $scope.searchSelection = $scope.options[0];
 
+        $scope.resetParams = function () {
+        	$scope.pagesize = 10;
+        	$scope.whichPage = 1;
+        	$scope.searchSelection = $scope.options[0];
+        };
 
+        $scope.resetParams();
 }]);

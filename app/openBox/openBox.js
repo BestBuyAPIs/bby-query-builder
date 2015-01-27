5e6b3ff11717ea0738c0fb37b91ff4df1089902b
Chris Lizama <christopher.lizama@gmail.com>
2015-01-27 15:37:14 -0600

    open box queries run properly

--- app/openBox/openBox.js
+++ app/openBox/openBox.js
@@ -7,6 +7,7 @@ angular.module('bby-query-mixer.openBox').controller('openBoxCtrl', [
     function ($scope, $resource, categoryConfig) {
         $scope.categories = angular.copy(categoryConfig);
         $scope.category = $scope.categories[0];
+        
         var httpClient = function (query) {
             return $resource(query, {}, {
                 jsonp_query: {
@@ -16,19 +17,55 @@ angular.module('bby-query-mixer.openBox').controller('openBoxCtrl', [
         };
 
         $scope.options = [
-            {text: "Select an Open Box Search Option", value:0},
-        	{text: "Open Box Offers All SKUs", value:1},
-        	{text: "Open Box Offers by Category", value:2},
-        	{text: "Open Box Offers by List of SKUs", value:3},
-        	{text: "Open Box Offers by SKU", value:4}
+            { text: "Select an Open Box Search Option", value: 0 },
+        	{ text: "Open Box Offers All SKUs", value: 'allSkus' },
+        	{ text: "Open Box Offers by Category", value: 'category' },
+        	{ text: "Open Box Offers by List of SKUs", value: 'skuList' },
+        	{ text: "Open Box Offers by SKU", value: 'singleSku' }
         ];
         
+        $scope.buildRemixQuery = function () {
+            var baseUrl = 'http://api.bestbuy.com/beta/products/openBox'
+            var categoryQuery = (($scope.searchSelection.value === 'category')&& $scope.category.value) ? baseUrl += '(categoryId='+$scope.category.value+')' :'';
+            var skuListQuery = (($scope.searchSelection.value === 'skuList')&&($scope.skuList)) ? baseUrl += '(sku%20in('+$scope.skuList+')':'';
+            var singleSkuQuery = (($scope.searchSelection.value === 'singleSku')&&($scope.singleSku)) ? baseUrl = 'http://api.bestbuy.com/beta/products/'+$scope.singleSku +'/openBox' : '';
+            var apiKey = $scope.apiKey ? baseUrl += '?apiKey='+$scope.apiKey : '';
+            baseUrl += '&callback=JSON_CALLBACK';
+            return baseUrl
+        };
+
+        $scope.invokeRemixQuery = function () {
+            $scope.results = "Running";
+            var query = $scope.buildRemixQuery();
+            var successFn = function (value) {
+                $scope.results = value;
+            };
+            var errorFn = function (httpResponse) {
+                $scope.results = httpResponse;
+            };
+
+            if (($scope.apiKey !==  "")&($scope.searchSelection.value !== 0)){
+                $scope.errorResult = false;
+                httpClient(query).jsonp_query(successFn, errorFn);
+            }else if ($scope.apiKey ===  ""){
+                $scope.errorResult = true;
+                $scope.results = "Please enter your API Key";
+            } else{
+                $scope.errorResult = true;
+                $scope.results = "Please pick a search option";
+            };
+        };
+
         $scope.searchSelection = $scope.options[0];
 
         $scope.resetParams = function () {
+            $scope.errorResult = false;
+            $scope.results = {};
         	$scope.pagesize = 10;
         	$scope.whichPage = 1;
         	$scope.searchSelection = $scope.options[0];
+            $scope.skuList = '';
+            $scope.singleSku = '';
         };
 
         $scope.resetParams();

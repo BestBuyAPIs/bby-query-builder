9b058a63d1b3e183474a052c47b0d10aa7dc71c4
Chris Lizama <christopher.lizama@gmail.com>
2015-01-27 10:17:29 -0600

    add drop down for open box search options

--- app/openBox/openBox.js
+++ app/openBox/openBox.js
@@ -5,7 +5,8 @@ angular.module('bby-query-mixer.openBox').controller('openBoxCtrl', [
     '$resource',
     'categoryConfig',
     function ($scope, $resource, categoryConfig) {
-
+        $scope.categories = angular.copy(categoryConfig);
+        $scope.category = $scope.categories[0];
         var httpClient = function (query) {
             return $resource(query, {}, {
                 jsonp_query: {
@@ -15,11 +16,11 @@ angular.module('bby-query-mixer.openBox').controller('openBoxCtrl', [
         };
 
         $scope.options = [
-            {text: "Select an Open Box Search Option"},
-        	{text: "Open Box Offers All SKUs"},
-        	{text: "Open Box Offers by Category"},
-        	{text: "Open Box Offers by List of SKUs"},
-        	{text: "Open Box Offers by SKU"}
+            {text: "Select an Open Box Search Option", value:0},
+        	{text: "Open Box Offers All SKUs", value:1},
+        	{text: "Open Box Offers by Category", value:2},
+        	{text: "Open Box Offers by List of SKUs", value:3},
+        	{text: "Open Box Offers by SKU", value:4}
         ];
         
         $scope.searchSelection = $scope.options[0];

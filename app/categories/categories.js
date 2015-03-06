f36657d213ef02e4e74d80af0661939ea228e018
Chris Lizama <christopher.lizama@gmail.com>
2015-03-06 11:25:18 -0600

    add basic query invocation function

--- app/categories/categories.js
+++ app/categories/categories.js
@@ -8,6 +8,7 @@ angular.module('bby-query-mixer.categories').controller('CategoriesCtrl', [
     function ($scope, HttpClientService, GaService, categoryResponseConfig) {
 
         $scope.searchOptions = [
+            {text:"Choose a seach option", value:""},        
             {text:"All Categories", value:"allcategories"},
             {text:"Top Level Categories", value:"toplevelcategories"},
             {text:"Search By Category Name", value:"categoryname"},
@@ -16,14 +17,45 @@ angular.module('bby-query-mixer.categories').controller('CategoriesCtrl', [
         ];
 
         $scope.buildRemixQuery = function () {
-           
-        };
+            var queryUrl = 'http://api.remix.bestbuy.com/v1/categories';
 
-        $scope.invokeRemixQuery = function () {
+            
+            var queryParams = '?';
+            var addKey = $scope.apiKey ? queryParams += 'apiKey=' + $scope.apiKey : '';
+            queryParams += '&format=json&callback=JSON_CALLBACK';
+
+            return queryUrl + queryParams;
         };
 
         $scope.buildParams = function () {
            
+        };
+
+        $scope.invokeRemixQuery = function () {
+            $scope.results = "Running";
+            var query = $scope.buildRemixQuery();
+
+            var successFn = function (value) {
+                $scope.results = value;
+            };
+            var errorFn = function (httpResponse) {
+                $scope.results = httpResponse;
+            };
+
+            if (($scope.apiKey !==  "")&($scope.searchSelection.value !== '')){
+                $scope.errorResult = false;
+
+                var eventActionName = "categories query success";
+                GaService.clickQueryButtonEvent(eventActionName, $scope.apiKey);
+
+                HttpClientService.httpClient(query).jsonp_query(successFn, errorFn);
+            }else if ($scope.apiKey ===  ""){
+                $scope.errorResult = true;
+                $scope.results = "Please enter your API Key";
+            } else {
+                $scope.errorResult = true;
+                $scope.results = "Please pick a search option";
+            };
         };
 
         $scope.categoryResponse = {};

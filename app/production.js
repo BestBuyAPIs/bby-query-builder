ae4447a23d3033bbe3d621ae990da92c03a20ced
Chris Lizama <christopher.lizama@gmail.com>
2015-03-25 14:30:10 -0500

    reviews service function into production.js

--- app/production.js
+++ app/production.js
@@ -1286,6 +1286,54 @@ angular.module('bby-query-mixer.productSearch').constant('reviewAttributeOptions
 ]);
 'use strict';
 
+angular.module('bby-query-mixer.reviews').factory('ReviewServices', [ 'restrictedReviewSortOptions',function(restrictedReviewSortOptions) {
+    
+    var preSelectOperator = function(form) {
+        form.opt = form.value.operator[0];
+        form.complexVal = form.value.valueOptions ? form.value.valueOptions[0].value : '';
+    };
+
+    var addAllShowOptions = function(optionArray) {
+        var newArray = [];
+        angular.forEach(optionArray, function(i) { this.push(i.value) }, newArray);
+        return newArray.join(',');
+    };
+
+    var parseDynamicForms = function (array) {
+            var newArray = [];
+            angular.forEach(array, function(i) { 
+                if (i.value.productAttribute && i.opt.value && i.complexVal){
+                    if (i.opt.value === ' in ') {
+                        this.push(i.value.productAttribute + i.opt.value +'('+ i.complexVal+')'); 
+                    }else {
+                this.push(i.value.productAttribute + i.opt.value + i.complexVal); 
+                    }
+                }
+            }, newArray);
+
+            return newArray.join('&');
+    };
+
+    var restrictReviewsSortOptionLists = function (array) {
+        var newArray = [];
+        angular.forEach(array,
+            function(i) {
+                if (restrictedReviewSortOptions.indexOf(i.value) === -1){
+                    this.push(i)
+                }
+            }, newArray);
+        return newArray;
+    };
+
+    return {
+    	preSelectOperator : preSelectOperator,
+    	addAllShowOptions : addAllShowOptions,
+    	parseDynamicForms : parseDynamicForms,
+        restrictReviewsSortOptionLists : restrictReviewsSortOptionLists
+    }
+}]);
+'use strict';
+
 angular.module('bby-query-mixer.reviews').controller('ReviewsCtrl', [
     '$scope',
     'categoryConfig',
@@ -1374,4 +1422,8 @@ angular.module('bby-query-mixer.reviews').constant('reviewShowOptionsConfig', [
 	{ text: 'SKU', value: 'sku' },
 	{ text: 'Submission Time', value: 'submissionTime' },
 	{ text: 'Title', value: 'title' }
+])
+.constant('restrictedReviewSortOptions', [
+	'reviewer.name',
+	'aboutMe.customerType'
 ]);

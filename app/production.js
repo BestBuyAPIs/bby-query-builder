7fb0bc8c81248d30df82736b779dac0546e13186
Chris Lizama <christopher.lizama@gmail.com>
2015-03-25 12:29:28 -0500

    start working on review search forms

--- app/production.js
+++ app/production.js
@@ -1276,16 +1276,36 @@ angular.module('bby-query-mixer.reviews', ['ngRoute'])
 );
 'use strict';
 
+angular.module('bby-query-mixer.productSearch').constant('reviewAttributeOptionsConfig', [ 
+	{text:'Comment', reviewAttribute:'comment',operator:[{value:'='}],placeholder:'I bought this along with my laptop.' },
+	{text:'Id', reviewAttribute:'id',operator:[{value:'='}],placeholder:'24798186' },
+	{text:'Rating', reviewAttribute:'rating',operator:[{value:'='}],placeholder:'4.0' },
+	{text:'Reviewer', reviewAttribute:'reviewer.name',operator:[{value:'='}],placeholder:'1cowgirl', },
+	{text:'SKU', reviewAttribute:'sku',operator:[{value:'='}],placeholder:'3764993' },
+	{text:'Submission Time', reviewAttribute:'submissionTime',operator:[{value:'='}],placeholder:'2014-04-29T22:40:33' },
+	{text:'Title', reviewAttribute:'title',operator:[{value:'='}],placeholder:'Good keyboard' }
+]);
+'use strict';
+
 angular.module('bby-query-mixer.reviews').controller('ReviewsCtrl', [
     '$scope',
     'categoryConfig',
     'HttpClientService',
     'GaService',
-    function ($scope, categoryConfig, HttpClientService, GaService) {
+    'ProductServices',
+    'reviewAttributeOptionsConfig',
+    function ($scope, categoryConfig, HttpClientService, GaService, ProductServices, reviewAttributeOptionsConfig) {
         
+        $scope.attributeOptions = angular.copy(reviewAttributeOptionsConfig);
+        
+        $scope.dynamicForms = [{id: '0',value:'',opt:'',complexVal:''}];
+
         $scope.resetReviewsQuery = function () {
             $scope.whichPage = 1;
             $scope.pageSize = 10;
         };
         $scope.resetReviewsQuery();
+
+        $scope.parseDynamicForms = ProductServices.parseDynamicForms();
+        $scope.preselectOperator = ProductServices.preSelectOperator;
 }]);
--- app/reviews/reviews.js
+++ app/reviews/reviews.js
@@ -5,11 +5,20 @@ angular.module('bby-query-mixer.reviews').controller('ReviewsCtrl', [
     'categoryConfig',
     'HttpClientService',
     'GaService',
-    function ($scope, categoryConfig, HttpClientService, GaService) {
+    'ProductServices',
+    'reviewAttributeOptionsConfig',
+    function ($scope, categoryConfig, HttpClientService, GaService, ProductServices, reviewAttributeOptionsConfig) {
         
+        $scope.attributeOptions = angular.copy(reviewAttributeOptionsConfig);
+        
+        $scope.dynamicForms = [{id: '0',value:'',opt:'',complexVal:''}];
+
         $scope.resetReviewsQuery = function () {
             $scope.whichPage = 1;
             $scope.pageSize = 10;
         };
         $scope.resetReviewsQuery();
+
+        $scope.parseDynamicForms = ProductServices.parseDynamicForms();
+        $scope.preselectOperator = ProductServices.preSelectOperator;
 }]);

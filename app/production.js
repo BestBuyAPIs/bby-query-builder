35e87d2dea3caaa996b15390019c7ba8413a9922
Chris Lizama <christopher.lizama@gmail.com>
2015-03-26 12:55:20 -0500

    add more operators for ratings

--- app/production.js
+++ app/production.js
@@ -1276,13 +1276,13 @@ angular.module('bby-query-mixer.reviews', ['ngRoute'])
 
 angular.module('bby-query-mixer.productSearch').constant('reviewAttributeOptionsConfig', [ 
 	{text:'Choose review attributes', reviewAttribute:false,operator:false,placeholder:false},
-	{text:'Comment', reviewAttribute:'comment',operator:[{value:'='}],placeholder:'I <3 this phone.' },
-	{text:'Id', reviewAttribute:'id',operator:[{value:'='}],placeholder:'24798186' },
-	{text:'Rating', reviewAttribute:'rating',operator:[{value:'='}],placeholder:'4.0' },
-	{text:'Reviewer', reviewAttribute:'reviewer.name',operator:[{value:'='}],placeholder:'BBY-Fan28' },
-	{text:'SKU', reviewAttribute:'sku',operator:[{value:'='}],placeholder:'3764993' },
-	{text:'Submission Time', reviewAttribute:'submissionTime',operator:[{value:'='}],placeholder:'2014-04-29 T22:40:33' },
-	{text:'Title', reviewAttribute:'title',operator:[{value:'='}],placeholder:'Good keyboard' }
+	{text:'Comment', reviewAttribute:'comment',operator:[{value:'='}],placeholder:'I <3 this phone.', type:'string' },
+	{text:'Id', reviewAttribute:'id',operator:[{value:'='}],placeholder:'24798186', type:'string' },
+	{text:'Rating', reviewAttribute:'rating',operator:[{value:'='},{value:'>'},{value:'<'},{value:'<='},{value:'>='}],placeholder:'4.0', type:'number' },
+	{text:'Reviewer', reviewAttribute:'reviewer.name',operator:[{value:'='}],placeholder:'BBY-Fan28', type:'string' },
+	{text:'SKU', reviewAttribute:'sku',operator:[{value:'='}],placeholder:'3764993', type:'string' },
+	{text:'Submission Time', reviewAttribute:'submissionTime',operator:[{value:'='}],placeholder:'2014-04-29 T22:40:33', type:'string' },
+	{text:'Title', reviewAttribute:'title',operator:[{value:'='}],placeholder:'Good keyboard', type:'string' }
 ]);
 'use strict';
 

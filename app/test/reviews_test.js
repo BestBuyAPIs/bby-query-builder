0bb9863933f5ad307a3aea2e3800715471ab7c72
Chris Lizama <christopher.lizama@gmail.com>
2015-03-26 15:04:35 -0500

    increase test coverage from 65% of lines to 86%

--- app/test/reviews_test.js
+++ app/test/reviews_test.js
@@ -5,24 +5,46 @@ describe('bby-query-mixer.reviews module', function () {
     beforeEach(module('bby-query-mixer.reviews', 'appConfig'));
 
     describe('reviews controller', function () {
-        var ctrl, scope, ga;
-        beforeEach(inject(function ($controller, $rootScope) {
+        var ctrl, scope, ga, ReviewServices;
+        beforeEach(inject(function ($controller, $rootScope, _ReviewServices_) {
             scope = $rootScope.$new();
             ctrl = $controller('ReviewsCtrl', {
                 $scope: scope
             });
+            ReviewServices = _ReviewServices_;
             ga = (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
                 (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
                     m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
                     })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
             }));
-
+        beforeEach(inject(function ($controller, $rootScope) {
+            scope.apiKey = 'testKey';
+        
+        }));
         describe('build reviews query function', function () {
             it('should be defined', function () {
                 expect(scope.buildReviewsQuery).toBeDefined();
             });
-            it('should add the appropriate endpoint', function (){
-                expect(scope.buildReviewsQuery()).toEqual('https://api.remix.bestbuy.com/v1/reviews&callback=JSON_CALLBACK&format=json');
+            it('should build the right base url', function (){
+                expect(scope.buildReviewsQuery()).toEqual('https://api.remix.bestbuy.com/v1/reviews?apiKey=testKey&callback=JSON_CALLBACK&format=json');
+            });
+            it('should be add apikey if present', function () {
+                scope.apiKey = '123456';
+                expect(scope.buildReviewsQuery()).toEqual('https://api.remix.bestbuy.com/v1/reviews?apiKey=123456&callback=JSON_CALLBACK&format=json');
+            });
+            it('should be add pagination if it is not default value', function () {
+                scope.whichPage = '12';
+                scope.pageSize = '12';
+                expect(scope.buildReviewsQuery()).toEqual('https://api.remix.bestbuy.com/v1/reviews?apiKey=testKey&pageSize=12&page=12&callback=JSON_CALLBACK&format=json');
+            });
+            it('should add sort options if needed', function () {
+                scope.sortBy = "rating";
+                scope.sortOrder.value = 'asc';
+                expect(scope.buildReviewsQuery()).toEqual('https://api.remix.bestbuy.com/v1/reviews?apiKey=testKey&callback=JSON_CALLBACK&format=json');
+            });
+            it('should add list of different search paramters', function () {
+                scope.dynamicForms = [ {value:{reviewAttribute:'test'}, opt:{value:'='}, complexVal:"bar" },{value:{reviewAttribute:'foo'}, opt:{value:'='}, complexVal:"apple" } ];
+                expect(scope.buildReviewsQuery()).toEqual('https://api.remix.bestbuy.com/v1/reviews(test=bar&foo=apple)?apiKey=testKey&callback=JSON_CALLBACK&format=json');
             });
         });
         describe('invoke recommendations query function', function (){
@@ -44,9 +66,37 @@ describe('bby-query-mixer.reviews module', function () {
                 scope.resetReviewsQuery();
                 expect(scope.results).toEqual({});
                 expect(scope.errorResult).toEqual(false);
+                expect(scope.whichPage).toEqual(1);
+                expect(scope.pageSize).toEqual(10);
+                expect(scope.showOption.list).toEqual([]);
+
             });
         });
-
+        describe('select all button', function () {
+            it('should select all', function () {
+                scope.selectAll('allreviews')
+                expect(scope.showOption.list.length).toEqual(7);
+            });
+            it('should select none', function () {
+                scope.selectAll('none')
+                expect(scope.showOption.list.length).toEqual(0);
+            });
+        });
+        describe('restrict sort options function', function () {
+            it('should return an array without the values on the restricted list', function () {
+                var firstArray = [{value:'reviewer.name'},{value:'rating'},{value:'comment'}];
+                var secondArray = ReviewServices.restrictReviewsSortOptionLists(firstArray);
+                expect(secondArray).toEqual([ { value : 'rating' }, { value : 'comment' } ]);
+            });
+        });
+        describe('clearBlankSelect function', function () {
+            it('should set sortBy and sortOptions correctly', function () {
+                scope.showOption.list = [{value:'reviewer.name'},{value:'rating'},{value:'comment'}];
+                scope.clearBlankSelect();
+                expect(scope.sortOptions.list).toEqual( [{value:'rating'},{value:'comment'}]);
+                expect(scope.sortBy).toEqual({ value : 'rating' });
+            });
+        });
 
     });
 });

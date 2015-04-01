03fa49086f389932c665792acde9acb2e7db084e
Chris Lizama <christopher.lizama@gmail.com>
2015-04-01 14:19:01 -0500

    _built files at Wed Apr  1 14:19:01 CDT 2015

--- _built/productSearch/productServices.js
+++ _built/productSearch/productServices.js
@@ -2,24 +2,13 @@
 
 angular.module('bby-query-mixer.productSearch').factory('ProductServices', [ 'restrictedSortOptions',function(restrictedSortOptions) {
     
-    var preSelectOperator = function(form) {
-        form.opt = form.value.operator[0];
-        form.complexVal = form.value.valueOptions ? form.value.valueOptions[0].value : '';
-    };
-
-    var addAllShowOptions = function(optionArray) {
-        var newArray = [];
-        angular.forEach(optionArray, function(i) { this.push(i.value) }, newArray);
-        return newArray.join(',');
-    };
-
     var parseDynamicForms = function (array) {
             var newArray = [];
             angular.forEach(array, function(i) { 
                 if (i.value.productAttribute && i.opt.value && i.complexVal){
                     if (i.opt.value === ' in ') {
                         this.push(i.value.productAttribute + i.opt.value +'('+ i.complexVal+')'); 
-                    }else {
+                    } else {
                 this.push(i.value.productAttribute + i.opt.value + i.complexVal); 
                     }
                 }
@@ -46,8 +35,6 @@ angular.module('bby-query-mixer.productSearch').factory('ProductServices', [ 're
     };
 
     return {
-    	preSelectOperator : preSelectOperator,
-    	addAllShowOptions : addAllShowOptions,
     	parseDynamicForms : parseDynamicForms,
     	addAllOptionValues : addAllOptionValues,
         restrictSortOptionLists : restrictSortOptionLists

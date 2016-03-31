dd565bde3775c12fb49d74cc47cfc2543ba2661d
Leon Giles <leon.giles@bestbuy.com>
2016-03-31 13:41:07 -0500

    Remove reviews endpoint, tab and tests
    [COMIX-1121]

--- karma.conf.js
+++ karma.conf.js
@@ -45,8 +45,7 @@ module.exports = function (config) {
             'app/productSearch/*.js': ['coverage'],
             'app/recommendations/*.js': ['coverage'],
             'app/smartLists/*.js': ['coverage'],
-            'app/stores/*.js': ['coverage'],
-            'app/reviews/*.js': ['coverage']
+            'app/stores/*.js': ['coverage']
         },
         reporters: ['coverage','dots']
     });
